c926b32d8dfa139abc849192c2a0f15c2ad53fae
jaredwy <jared.wyles@gmail.com>
2010-06-05 17:17:35 +1000

    Added user tasks

---
+++ to-site/Models/Users.py
@@ -0,0 +1,22 @@
+import datetime
+import Tasks
+from google.appengine.ext import db
+from google.appengine.api import users
+
+
+class User(db.Model):
+    ID = db.IntegerProperty()
+    Name = db.StringProperty()
+    DateOfBirth = db.DateProperty()
+    Status = 
+    
+    
+class UserTraits:
+    User = db.ReferenceProperty(User, collection_name='UserTraits')
+    Points = db.IntegerProperty()
+    Trait = TaskType()
+ 
+class UserTasks(db.Model):
+    Own = db.BooleanProperty()
+    User = db.ReferenceProperty(User,collection_name='UserTasks')
+    
