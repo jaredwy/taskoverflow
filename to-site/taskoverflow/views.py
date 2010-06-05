e8affb6e096bc9e689ab1eb895b083ac4790c95e
jaredwy <jared.wyles@gmail.com>
2010-06-05 17:55:46 +1000

    some changes to the model to associate a friendly name to the task

--- to-site/taskoverflow/views.py
+++ to-site/taskoverflow/views.py
@@ -1,12 +1,12 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
+import models
 def task_view(request, task_id):
     return HttpResponse("Viewing task")
     
 def task_new(request):
-    return HttpResponse("Creating task")
+    return HttpResponse("Creating task" + newTask.ID)
     
 def user_view(request, user_id):
     return HttpResponse("Showing user")
