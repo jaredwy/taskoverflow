aa90d988d2e02f6e115e40ed9d68f4da5c7f87d9
Pamela Fox <pamela.fox@gmail.com>
2010-06-05 17:19:09 +1000

    Adding URLs + views

--- to-site/taskoverflow/views.py
+++ to-site/taskoverflow/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+def task_view(request, task_id):
+    return HttpResponse("Viewing task")
+    
+@login_required
+def task_new(request, task_id):
+    return HttpResponse("Creating Views")
