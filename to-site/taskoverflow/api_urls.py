f5eb1cb3563300d9d35c3529d07131790d2e59eb
jaredwy <jared.wyles@gmail.com>
2010-06-06 13:23:53 +1000

    replacing removed files

--- to-site/taskoverflow/api_urls.py
+++ to-site/taskoverflow/api_urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('taskoverflow.api_views',
     (r'task/update/(?P<key>.+)*$', 'task_update'),
     (r'task/create', 'task_create'),
-)
+) + patterns('taskoverflow.api_search',
+    r'task/search','task_search')
